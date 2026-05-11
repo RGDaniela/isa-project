67460b463b5ae195f4a9e2d493946dffebe82f36
RGDaniela <luzdeluna08@outlook.es>
2026-05-11 02:10:27 +0000

    Agregado base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/isa-project/'
+})git add .
+git commit -m "Agregado base para GitHub Pages"
+git push origin main
